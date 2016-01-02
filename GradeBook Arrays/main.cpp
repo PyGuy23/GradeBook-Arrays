303d54d5919fa4af2be9a8ae8394bb73e91445af
Timothy <eclisesolaris23@gmail.com>
2016-01-02 12:57:11 -0500

    Initial Commit

---
+++ GradeBook Arrays/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GradeBook Arrays
+//
+//  Created by newuser on 1/2/16.
+//  Copyright © 2016 Gradebook Arrays. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
